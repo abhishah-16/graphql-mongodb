b7ebfd4aacaafaa35773d7cd4a87e3cd09fb98b3
abhishah <abhishah0961@gmail.com>
2022-10-11 14:23:36 +0530

    modify: resolvers

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,9 @@
+const postResolvers = require('./post')
+const userResolvers = require('./user')
+
+module.exports = {
+    Query: {
+        ...postResolvers.Query,
+        ...userResolvers.Query
+    }
+}
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server')
 const mongoose = require('mongoose')
 
 const typeDefs = require('./graphql/typeDefs')
-const resolvers = require('./graphql/resolvers')
+const resolvers = require('./graphql/resolvers/index')
 
 const server = new ApolloServer({ typeDefs, resolvers })
 
