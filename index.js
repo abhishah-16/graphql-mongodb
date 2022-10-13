4bcdc512c91855b91ef3eb24dbe47999417346d8
abhishah <abhishah0961@gmail.com>
2022-10-13 15:34:15 +0530

    Refactor: plugin

--- index.js
+++ index.js
@@ -6,45 +6,14 @@ const mongoose = require('mongoose')
 
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers/index')
+const myPlugin = require('./plugin')
 const pubsub = new PubSub()
-
-const myPlugin = {
-    async requestDidStart(initialRequestContext) {
-        return {
-            async executionDidStart(executionRequestContext) {
-                return {
-                    willResolveField({ source, args, contextValue, info }) {
-                        const start = process.hrtime.bigint();
-                        return (error, result) => {
-                            console.log(result);
-                            const end = process.hrtime.bigint();
-                            if (info.parentType.name == 'Mutation') {
-                                console.log(
-                                    `Field ${info.parentType.name}.${info.fieldName
-                                        }`.yellow,
-                                );
-                                if (error) {
-                                    console.log(`It failed with ${error}`.magenta);
-                                } else {
-                                    console.log(`It returned ${result}`.green);
-                                }
-                            }
-                        };
-                    },
-                };
-            },
-        };
-    },
-}
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: ({ req }) => ({ req, pubsub }),
     plugins: [myPlugin],
-    subscription: {
-        'graphql-ws': true
-    }
 })
 
 mongoose.connect(process.env.DATABASE_URL, () => {
