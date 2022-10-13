b1289b14478d30c13f94f24d147507b68f5fb2a6
abhishah <abhishah0961@gmail.com>
2022-10-13 11:08:37 +0530

    Added: apollo plugin

--- index.js
+++ index.js
@@ -5,10 +5,37 @@ const mongoose = require('mongoose')
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers/index')
 
+const myPlugin = {
+    async requestDidStart(initialRequestContext) {
+        return {
+            async executionDidStart(executionRequestContext) {
+                return {
+                    willResolveField({ source, args, contextValue, info }) {
+                        const start = process.hrtime.bigint();
+                        return (error, result) => {
+                            const end = process.hrtime.bigint();
+                            console.log(
+                                `Field ${info.parentType.name}.${info.fieldName
+                                } took ${end - start}ns`,
+                            );
+                            if (error) {
+                                console.log(`It failed with ${error}`);
+                            } else {
+                                console.log(`It returned ${result}`);
+                            }
+                        };
+                    },
+                };
+            },
+        };
+    },
+}
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => ({ req })
+    context: ({ req }) => ({ req }),
+    plugins: [myPlugin]
 })
 
 mongoose.connect(process.env.DATABASE_URL, () => {
