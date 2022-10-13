794f65eddc4efc47bcd78bbba6855f9cd611e5f3
abhishah <abhishah0961@gmail.com>
2022-10-13 12:29:09 +0530

    Modify: plugin

--- index.js
+++ index.js
@@ -15,14 +15,16 @@ const myPlugin = {
                         const start = process.hrtime.bigint();
                         return (error, result) => {
                             const end = process.hrtime.bigint();
-                            console.log(
-                                `Field ${info.parentType.name}.${info.fieldName
-                                    }`.yellow,
-                            );
-                            if (error) {
-                                console.log(`It failed with ${error}`.magenta);
-                            } else {
-                                console.log(`It returned ${result}`.green);
+                            if (info.parentType.name == 'Mutation') {
+                                console.log(
+                                    `Field ${info.parentType.name}.${info.fieldName
+                                        }`.yellow,
+                                );
+                                if (error) {
+                                    console.log(`It failed with ${error}`.magenta);
+                                } else {
+                                    console.log(`It returned ${result}`.green);
+                                }
                             }
                         };
                     },
